9e52188ebb96dcdf7f1c440a7dbad9c52723aaa8
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2021-11-10 16:44:10 +0100

    COMMIT 2.2 got module 2 database working! now add post to twitter

--- module2moderatie.py
+++ module2moderatie.py
@@ -28,20 +28,15 @@ def modifystatustweet(berichtOK, modnum):
         password="algra50")  # password from installation
     cur = con.cursor()
 
-
+    # insert modnum into moderator(moderator_id)
     insert_query = '''INSERT INTO moderator(moderator_id) VALUES (%s);'''
     record_to_insert = (modnum),
     cur.execute(insert_query, record_to_insert)
 
-
-
-
-
+    # fetch tweetnumber next unmoderated tweet
     cur.execute('''SELECT tweet_number FROM tweets WHERE tweet_status = 0 LIMIT 1''')
     record = cur.fetchone()
-    print('record: ',record)
     record_in_use = record[0]
-    print('rec in use',type(record_in_use),record_in_use)
 
     if berichtOK == "ja":
         insert_query_update_stat_yes = '''UPDATE tweets SET tweet_status = (%s) WHERE tweet_number = (%s);'''
@@ -65,24 +60,29 @@ def modifystatustweet(berichtOK, modnum):
         print('tweet goedgekeurd')
         return exit()
 
-
-
     elif berichtOK == "nee":
-        insert_query = '''update tweets set tweet_status = 1 where tweet_number = (%s);'''
-        record_to_insert = int(record[0][0]),
+        insert_query_update_stat_no = '''UPDATE tweets SET tweet_status = (%s) WHERE tweet_number = (%s);'''
+        record_to_insert_stat_no = (1, record_in_use)
+
+        cur.execute(insert_query_update_stat_no, record_to_insert_stat_no)
+
+        con.commit()
+
+        insert_query_update_moddate = '''UPDATE tweets SET moderation_date = CURRENT_TIMESTAMP WHERE tweet_number = %s;'''
+        cur.execute(insert_query_update_moddate % (record_in_use))
+        con.commit()
+
+        insert_query = '''UPDATE tweets SET moderator_id_fk = %s WHERE tweet_number = %s; '''
+        record_to_insert = (modnum, record_in_use)
         cur.execute(insert_query, record_to_insert)
         con.commit()
+
         cur.close()
         con.close()
-        print('tweet afgekeurd')
+        print('tweet goedgekeurd')
         return exit()
-
-    # --------------------------------------
-    # update line with mod number
-    # --------------------------------------
-
 
     cur.close()
     con.close()
 
-    return exit()
+    return print('yellow')
